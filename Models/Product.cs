9d8ba8b2ce108641616714e6e7d4b79b5b58468a
Mohamed Abdelmageed <mohamedabdelmageed8@outlook.com>
2019-11-15 10:07:40 +0200

    product

---
+++ Models/Product.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FashionSales.Models
+{
+    public class Product
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+
+        public string Image { get; set; }
+
+        public float Price { get; set; }
+
+        public int Quantity { get; set; }
+
+        public int CategoryId { get; set; }
+
+        public int ProviderId { get; set; }
+
+        public ICollection<OrderProduct> OrderProducts { get; set; }
+
+
+
+
+    }
+}
