9a045eb2567b13f6c17cd5fa9eebab30303c8e42
Mohamed Abdelmageed <mohamedabdelmageed8@outlook.com>
2019-11-15 10:08:57 +0200

    order and product (many to many) revise this ya basma

---
+++ Models/OrderProduct.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FashionSales.Models
+{
+    public class OrderProduct
+    {
+        public int ProductId { get; set; }
+        public int OrderId { get; set; }
+
+        public int Quantity { get; set; }
+        public Order Order { get; set; }
+        public  Product Product { get; set; }
+
+}
+}
