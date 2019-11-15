3c08661cb7b116689201b7804cd42ccc27713fa8
Mohamed Abdelmageed <mohamedabdelmageed8@outlook.com>
2019-11-15 10:06:39 +0200

    customer class

---
+++ Models/Customer.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FashionSales.Models
+{
+    public class Customer
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string UserName { get; set; }
+
+        public string Password { get; set; }
+
+
+        public string Location { get; set; }
+
+        public string Address { get; set; }
+
+        public int MobileNum { get; set; }
+        public string Description { get; set; }
+
+
+
+    }
+}
