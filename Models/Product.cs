36733274db91c447f4edcff4bf0cd3ac4bfa8794
BASMA <basmaebraheim@gmail.com>
2019-11-15 15:43:46 +0200

    product user relation

--- Models/Product.cs
+++ Models/Product.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -18,7 +19,9 @@ namespace FashionSales.Models
 
         public int CategoryId { get; set; }
 
+        [ForeignKey("Provider")]
         public int ProviderId { get; set; }
+        public Provider Provider { get; set; }
 
         public ICollection<OrderProduct> OrderProducts { get; set; }
 
