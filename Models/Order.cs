e1d1300f9db7c93021cf5c246d8ea00aa1329255
Mohamed Abdelmageed <mohamedabdelmageed8@outlook.com>
2019-11-15 10:08:21 +0200

    order

---
+++ Models/Order.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FashionSales.Models
+{
+    public class Order
+    {
+        public int Id { get; set; }
+        public int CustomerId { get; set; }
+        public int ProviderId { get; set; }
+
+        public float TotalPrice { get; set; }
+
+        public DateTime Date { get; set; }
+
+        public ICollection< OrderProduct> OrderProducts { get; set; }
+
+    }
+}
