0dfefcb7d54a7fee3fbe8c861e49ba45a0fb2f30
BASMA <basmaebraheim@gmail.com>
2019-11-15 13:46:18 +0200

    add datacontext

---
+++ Data/Seed.cs
@@ -0,0 +1,55 @@
+using System.Collections.Generic;
+using System.Linq;
+using FashionSales.Models;
+using Microsoft.AspNetCore.Identity;
+using Newtonsoft.Json;
+
+namespace FashionSales.Data
+{
+    public class Seed
+    {
+        private readonly UserManager<User> _userManager;
+        private readonly RoleManager<Role> _roleManager;
+
+        public Seed(UserManager<User> userManager, RoleManager<Role> roleManager)
+        {
+            _userManager = userManager;
+            _roleManager = roleManager;
+        }
+
+        public void SeedUsers()
+        {
+            if (!_userManager.Users.Any())
+            {
+                
+                var roles = new List<Role>
+                {
+                    new Role{Name = "Admin"},
+                    new Role{Name = "Student"},
+                    new Role{Name = "Instructor"},
+                };
+
+                foreach (var role in roles)
+                {
+                    _roleManager.CreateAsync(role).Wait();
+                }
+
+             
+
+                var adminUser = new User
+                {
+                    UserName = "Admin"
+                };
+
+                IdentityResult result = _userManager.CreateAsync(adminUser, "pass@123").Result;
+
+                if (result.Succeeded)
+                {
+                    var admin = _userManager.FindByNameAsync("Admin").Result;
+                    _userManager.AddToRolesAsync(admin, new[] { "Admin" }).Wait();
+                }
+                
+            }
+        }
+    }
+}
