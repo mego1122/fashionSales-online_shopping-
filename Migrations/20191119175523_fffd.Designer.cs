c807bb69dc21a6ab6a57a3bc7b2679ea7b5f65ae
Mohamed Abdelmageed <mohamedabdelmageed8@outlook.com>
2019-11-23 00:23:59 +0200

    n

--- Migrations/20191119175523_fffd.Designer.cs
+++ Migrations/20191119175523_fffd.Designer.cs
@@ -4,14 +4,16 @@ using FashionSales.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace FashionSales.Migrations
 {
     [DbContext(typeof(DataContext))]
-    partial class DataContextModelSnapshot : ModelSnapshot
+    [Migration("20191119175523_fffd")]
+    partial class fffd
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
